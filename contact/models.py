9705deb7fbaa2f543429bec894f2de173aee8196
Yuri Leal da Cruz <yakirodmorfera@gmail.com>
2024-03-05 19:49:46 -0300

    Atualização

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contacts(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
